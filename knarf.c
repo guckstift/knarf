af0a5801d5ee3377908a7a9e2eb7464d6c7c0d16
guckstift <guckstift@posteo.de>
2017-12-28 23:01:49 +0100

    Add ram. Add 4 operations.

--- knarf.c
+++ knarf.c
@@ -1,6 +1,48 @@
 #include <stdio.h>
 
+enum {HALT, PUSH, ADD, SUB};
+
+int ram[1024] = {
+	PUSH, 11,
+	PUSH, 13,
+	ADD,
+	PUSH, 4,
+	SUB,
+	HALT,
+};
+
+int *ip = ram;
+int *sp = ram + 1024;
+int running = 1;
+int op = 0;
+int op1 = 0;
+int op2 = 0;
+
 void main()
 {
 	printf("knarf\n");
+	
+	while(running) {
+		op = *ip++;
+		
+		switch(op) {
+			case HALT:
+				running = 0;
+				break;
+			case PUSH:
+				op1 = *ip++;
+				*--sp = op1;
+				break;
+			case ADD:
+				op2 = *sp++;
+				op1 = *sp++;
+				*--sp = op1 + op2;
+				break;
+			case SUB:
+				op2 = *sp++;
+				op1 = *sp++;
+				*--sp = op1 - op2;
+				break;
+		}
+	}
 }
