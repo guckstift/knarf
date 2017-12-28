8383ce8d6a14100429d35b7587559532155ecd73
guckstift <guckstift@posteo.de>
2017-12-28 23:21:36 +0100

    Implement Load and Store.

--- knarf.c
+++ knarf.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
-enum {HALT, PUSH, ADD, SUB, PRINT};
+enum {HALT, PUSH, LOAD, STORE, ADD, SUB, PRINT};
 
 int ram[1024] = {
+	PUSH, 512,
 	PUSH, 11,
 	PUSH, 13,
 	ADD,
 	PUSH, 4,
 	SUB,
+	STORE,
+	PUSH, 512,
+	LOAD,
 	PRINT,
 	HALT,
 };
@@ -33,6 +37,15 @@ void main()
 			case PUSH:
 				op1 = *ip++;
 				*--sp = op1;
+				break;
+			case LOAD:
+				op1 = *sp++;
+				*--sp = ram[op1];
+				break;
+			case STORE:
+				op2 = *sp++;
+				op1 = *sp++;
+				ram[op1] = op2;
 				break;
 			case ADD:
 				op2 = *sp++;
